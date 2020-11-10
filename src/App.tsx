a35fb01ff91a1fcc5dfa5b66d5f14b51fe2007c5
Nickchen Nick <nickchen120235@gmail.com>
2020-11-10 21:34:48 +0800

    create axis selectors

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,34 @@ import React, { useState } from 'react'
 import { AppBar, Toolbar, Typography, Paper, Button } from '@material-ui/core'
 
 import { CountryDialog } from './components/CountryDialog'
+import { AxisDialog } from './components/AxisDialog'
 
 import data from './utils/data'
 import styleClass from './utils/styles'
 import countryCode from './utils/countryCode'
+import seriesCode from './utils/seriesCode'
 
 const App = () => {
   const classes = styleClass()
 
   const [country, setCountry] = useState('CHN')
   const [countryOpen, setCountryOpen] = useState(false)
+  const [x, setX] = useState('')
+  const [xOpen, setXOpen] = useState(false)
+  const [y, setY] = useState('')
+  const [yOpen, setYOpen] = useState(false)
 
   const handleCountryClose = (value: string) => {
     setCountryOpen(false)
     setCountry(value)
+  }
+  const handleXClose = (value: string) => {
+    setXOpen(false)
+    setX(value)
+  }
+  const handleYClose = (value: string) => {
+    setYOpen(false)
+    setY(value)
   }
 
   return(
@@ -27,9 +41,13 @@ const App = () => {
       </AppBar>
       <Toolbar />
       <Paper className={classes.paper}>
-        <Button onClick={() => setCountryOpen(true)}>Select Country/Region: {countryCode[country]}</Button>
+        <Button onClick={() => setCountryOpen(true)}>Select Country/Region: {countryCode[country]}</Button><br/>
+        <Button onClick={() => setXOpen(true)}>Select X Axis: {seriesCode[x]}</Button>
+        <Button onClick={() => setYOpen(true)}>Select Y Axis: {seriesCode[y]}</Button>
       </Paper>
       <CountryDialog open={countryOpen} selected={country} onClose={handleCountryClose} />
+      <AxisDialog open={xOpen} selected={x} onClose={handleXClose} axis='x' />
+      <AxisDialog open={yOpen} selected={y} onClose={handleYClose} axis='y' />
     </>
   )
 }
---
+++ src/components/AxisDialog.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Dialog, DialogTitle, Container, Button, Typography } from '@material-ui/core'
+
+import seriesCode from '../utils/seriesCode'
+import styleClasses from '../utils/styles'
+
+interface AxisDialogProps {
+  open: boolean
+  selected: string
+  onClose: (value: string) => void
+  axis: 'x' | 'y'
+}
+
+export const AxisDialog = (props: AxisDialogProps) => {
+  const description = Object.values(seriesCode)
+  const { open, selected, onClose, axis } = props
+  const classes = styleClasses()
+
+  const handleClose = () => onClose(selected)
+  const handleClick = (name: string) => {
+    let value = Object.keys(seriesCode).find(key => seriesCode[key] === name)
+    if(value === undefined) value = ''
+    onClose(value)
+  }
+
+  return(
+    <Dialog onClose={handleClose} open={open} fullWidth maxWidth={false}>
+      <DialogTitle>Select {axis.toUpperCase()} Axis</DialogTitle>
+      <Container>
+        {description.map(value => <><Button key={value} onClick={() => handleClick(value)}><Typography align='left' variant='body2' noWrap>{value}</Typography></Button><br/></>)}
+      </Container>
+    </Dialog>
+  )
+}
