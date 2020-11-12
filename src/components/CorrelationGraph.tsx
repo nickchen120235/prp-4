8f21fb6c633dc9979450eaca9f0f55f113be5cd6
Nickchen Nick <nickchen120235@gmail.com>
2020-11-13 02:32:40 +0800

    add color to correlation graph

--- src/components/CorrelationGraph.tsx
+++ src/components/CorrelationGraph.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ResponsiveContainer, ScatterChart, CartesianGrid, XAxis, YAxis, Tooltip, Scatter } from 'recharts'
+import { ResponsiveContainer, ScatterChart, CartesianGrid, XAxis, YAxis, Tooltip, Scatter, Legend } from 'recharts'
 
 import data from '../utils/data'
 import seriesCode from '../utils/seriesCode'
@@ -11,26 +11,30 @@ interface CorrelationGraphProps {
   hasNaN: (value: boolean) => void
 }
 
+const years = ['2010', '2011', '2012', '2013', '2014']
+const colors = ['#003f5c', '#58508d', '#bc5090', '#ff6361', '#ffa600']
+
 export const CorrelationGraph = (props: CorrelationGraphProps) => {
   const { country, x, y, hasNaN } = props
-  const renderData = data[country][x].map((value, index) => ({
+  const renderData = data[country][x].map((value, index) => ([{
     x: value,
     y: data[country][y][index]
-  }))
+  }]))
 
   hasNaN(false)
   for (const i of renderData) {
-    if(isNaN(i.x) || isNaN(i.y)) hasNaN(true)
+    if(isNaN(i[0].x) || isNaN(i[0].y)) hasNaN(true)
   }
 
   return(
     <ResponsiveContainer aspect={1} width={500}>
-      <ScatterChart data={renderData}>
+      <ScatterChart>
         <CartesianGrid strokeDasharray='3 3' />
         <XAxis dataKey='x' name={seriesCode[x]} type='number' domain={['auto', 'auto']} />
         <YAxis dataKey='y' name={seriesCode[y]} type='number' domain={['auto', 'auto']} width={30} />
         <Tooltip />
-        <Scatter />
+        <Legend />
+        {renderData.map((value, index) => <Scatter name={years[index]} data={value} key={index} fill={colors[index]} animationDuration={250} />)}
       </ScatterChart>
     </ResponsiveContainer>
   )
